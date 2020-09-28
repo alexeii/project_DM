cdcdeb4d77a9391b1d023fd91998afada17d0732
Alexey Arhipiv <alex3171@list.ru>
2020-09-28 21:47:51 +0500

    feat:send form

--- src/modules/sendForm.js
+++ src/modules/sendForm.js
@@ -4,14 +4,48 @@ const sendForm = () => {
     const textMessage = popupThanks.querySelector('.form-content>p');
     const freeVisitForm = document.querySelector("#free_visit_form");
     const callbackForm = document.querySelector("#callback_form");
+    const checkboxInput = document.querySelectorAll("input[type='checkbox']");
+    const statusMessage = document.createElement("div");
+    statusMessage.textContent = "Подтвердите согласие на обработку данных.";
+    statusMessage.style.color = 'white';
     const errorMessage = "Что то пошло не так...";
     const loadMessage = "Идёт отправка...";
     const successMessage = "Ваша заявка отправлена.<br />Мы свяжемся с вами в ближайшее время.";
+    let count = 0;
+    let interval;
+
 
 
     form.forEach((item) => {
         item.addEventListener('submit', (event) => {
             event.preventDefault();
+            if (item.querySelector("input[type='checkbox']")) {
+                if (!item.querySelector("input[type='checkbox']").checked) {
+                    item.querySelector(".personal-data").append(statusMessage);
+                    const message = () => {
+                        if (statusMessage.textContent !== '!!!!') {
+                            statusMessage.textContent = '!!!!';
+
+                        } else if (statusMessage.textContent === '!!!!') {
+                            statusMessage.textContent = "Подтвердите согласие на обработку данных.";
+
+                        }
+                    };
+                    interval = setInterval(() => {
+                        count++;
+                        if (count === 8) {
+                            clearInterval(interval);
+                            statusMessage.remove();
+                            count = 0;
+                        } else {
+                            message();
+                        }
+                    }, 800);
+
+
+                    return;
+                }
+            }
             popupThanks.style.display = "block";
             freeVisitForm.style.display = "none";
             callbackForm.style.display = "none";
@@ -29,6 +63,9 @@ const sendForm = () => {
                     textMessage.innerHTML = successMessage;
                     event.target.querySelectorAll("input").forEach((item) => {
                         item.value = "";
+                    });
+                    checkboxInput.forEach((item) => {
+                        item.checked = false;
                     });
                     const timer = () => (popupThanks.style.display = "none");
                     setTimeout(timer, 2500);
--- src/modules/validateForm.js
+++ src/modules/validateForm.js
@@ -1,17 +1,28 @@
 const validateForm = () => {
-  const userNameValid = document.querySelectorAll("input[type='text']");
-  const userPhoneValid = document.querySelectorAll("input[type='tel']");
-  userNameValid.forEach((item) => {
-    item.addEventListener("keyup", () => {
-      item.value = item.value.replace(/[^а-я ]/gi, "");
+    const userNameValid = document.querySelectorAll("input[type='text']");
+    const userPhoneValid = document.querySelectorAll("input[type='tel']");
+    //валидация
+    for (let i = 0; i < userNameValid.length; i++) {
+
+        if (i !== 1) {
+            userNameValid[i].addEventListener("keyup", () => {
+                userNameValid[i].value = userNameValid[i].value.replace(/[^а-я ]/gi, "");
+            });
+        } else {
+            userNameValid[i].addEventListener("keyup", () => {
+                userNameValid[i].value = userNameValid[i].value.replace(/[^а-яА-Я0-9]/g, "");
+            });
+        }
+
+    };
+
+    userPhoneValid.forEach((item) => {
+        item.maxLength = 12;
+        item.addEventListener("keyup", () => {
+            item.value = item.value.replace(/[^\d\+]/g, "");
+        });
     });
-  });
-  userPhoneValid.forEach((item) => {
-    item.maxLength = 12;
-    item.addEventListener("keyup", () => {
-      item.value = item.value.replace(/[^\d\+]/g, "");
-    });
-  });
+
 };
 
 export default validateForm;
